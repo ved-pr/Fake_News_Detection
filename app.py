e2f0fc45bbef3429fa2762e5143b624fcfcd47a2
ved-pr <86920669+ved-pr@users.noreply.github.com>
2022-07-27 22:55:36 +0530

    Fake News Detection

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, escape, request, render_template
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vector = pickle.load(open("vectorizer.pkl", 'rb'))
+model = pickle.load(open("finalized_model.pkl", 'rb'))
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/prediction" , methods=['GET' , 'POST'])
+def prediction():
+  if request.method == "POST":
+    news = str(request.form['news'])
+    predict = model.predict(vector.transform([news]))[0]
+    print(predict)
+
+    return render_template("prediction.html" , prediction_text= "News headline is -> {}".format(predict))
+
+  else:
+    return render_template("prediction.html")
+
+if __name__ == "__main__":
+    app.run()
